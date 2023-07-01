1122d884dcd5c83e52e4f60ead0fc4adc3f2153c
EnverJ <Enlem1987@gmail.com>
2023-06-30 23:37:27 -0600

    loops

--- src/main/java/JavaBrain/loops.java
+++ src/main/java/JavaBrain/loops.java
@@ -1,4 +1,49 @@
 package JavaBrain;
 
 public class loops {
+    public void singleLoop() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+    }
+
+    public static void multiValueLoop() {
+        int i, j;
+        for (i = 0, j = 0; i < 10; j = 10, i++, j++) {
+            System.out.println(j * i);
+        }
+    }
+
+    // or
+    public static void multiLoop() {
+        int i, j;
+        for (i = 0, j = 10; i < 10; i++, j--) {
+            System.out.println(i * j);
+        }
+    }
+
+    public static void forEacLoop() {
+        int[] someNumbers = {2, 3, 4, 5, 0, 6, 7};
+        int total = 0;
+        for (var num : someNumbers) {
+            total = total + num;
+            System.out.println(total);
+        }
+    }
+
+    public static void nestedLoop() {
+        int[][] nums= new int[10][20];
+        for (int i = 0; i < 5; i++) {
+            for (int j= 0; j < 10 ; j++) {
+                System.out.println(i+j);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+    //multiValueLoop();
+    //multiLoop();
+      //  forEacLoop();
+        nestedLoop();
+    }
 }
