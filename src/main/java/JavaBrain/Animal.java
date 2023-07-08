590ea1ba0af576a3a13feba04b450572e8ce2d89
EnverJ <Enlem1987@gmail.com>
2023-07-07 22:39:50 -0600

    s

--- src/main/java/JavaBrain/Animal.java
+++ src/main/java/JavaBrain/Animal.java
@@ -1,5 +1,44 @@
 package JavaBrain;
 
+import static JavaBrain.cat.nonStop;
+
 // polymorphism
 public class Animal {
+    public void animalSound() {
+        System.out.println("Animals make sound");
+    }
+
+}
+
+class duck extends Animal {
+    public void animalSound() {
+        System.out.println("quak, quak, quak");
+    }
+}
+
+class cat extends Animal {
+    public void animalSound() {
+        System.out.println("meew, meew");
+    }
+    public  static void nonStop() {
+        while (true) {
+            System.out.println("I wont stop");
+            break; // if no break, i won't stop
+        }
+    }
+}
+
+
+class main {
+    public static void main(String[] args) {
+        Animal an = new Animal();
+        Animal dc = new duck();
+        Animal ct = new cat();
+        an.animalSound();
+        dc.animalSound();
+        ct.animalSound();
+        nonStop();
+    }
+
+
 }
--- src/main/java/JavaBrain/LocalClassVariable.java
+++ src/main/java/JavaBrain/LocalClassVariable.java
@@ -1,2 +1,15 @@
-package JavaBrain;public class LocalClassVariable {
+package JavaBrain;
+
+public class LocalClassVariable {
+
+    public static void main(String[] args) {
+       final int i = 10;
+        class Foo {
+            int x = i;
+
+
+        }
+    //        i = 20;
+        System.out.println(i);
+    }
 }
