75a6fa0928695e92e156954b98758838ca0e52ee
EnverJ <Enlem1987@gmail.com>
2023-07-18 21:47:31 -0600

    sets

---
+++ src/main/java/JavaBrain/collections/lists/LinkedListMain.java
@@ -0,0 +1,34 @@
+package JavaBrain.collections.lists;
+
+public class LinkedListMain {
+    /*
+    LinkedList performance
+    . get - o(N)
+    . set - o(N)
+    . contains - o(N)
+     add and remove
+      . remove(0) - o(1)
+      . add(0) - o(1)
+      . add - ?
+      . remove - ?
+
+      // separate the get from the add/remove
+      . get - o(n)
+      . add -o(1)
+      . remove - o(1)
+
+
+
+
+     */
+
+
+    /*
+    when to use what?
+    ArrayList vs LinkedList
+    . Position based access - ArrayList
+    . Constant adn random insertions and removal - LinkedList
+    . iterator- either
+
+     */
+}
---
+++ src/main/java/JavaBrain/collections/sets/setsMain.java
@@ -0,0 +1,14 @@
+package JavaBrain.collections.sets;
+
+public class setsMain {
+    /*
+    Sets
+    - A collection with no duplicate elements
+
+    Common use case:
+    - classifications, Assign student to lecture
+    ex:
+    A, B C
+
+     */
+}
