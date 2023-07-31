71be2576cb152a815a2dd157b7deaf3bdfcf9207
EnverJ <Enlem1987@gmail.com>
2023-07-30 22:15:29 -0600

    prime numebr

---
+++ src/main/java/JavaBrain/concurrency/B_PrimeNumberWithConcurrency.java
@@ -0,0 +1,26 @@
+package JavaBrain.concurrency;
+
+import java.util.Scanner;
+
+public class B_PrimeNumberWithConcurrency {
+    public static void main(String[] args) {
+        int number;
+        while (true) {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("\n I can tell you the nth prime number. Enter n: ");
+            int n = sc.nextInt();
+            if (n == 0) break;
+            Runnable r = new Runnable() {
+                @Override
+                public void run() {
+                    int number = PrimeNumberUtil.calculatePrime(n);
+                    System.out.println("\n Result:");
+                    System.out.println("\n value of " + n + "th prime: " + number);
+                }
+            };
+       Thread t = new Thread(r);
+       t.start();
+
+        }
+    }
+}
--- src/main/java/JavaBrain/concurrency/MyRunnable.java
+++ src/main/java/JavaBrain/concurrency/MyRunnable.java
@@ -1,2 +1,20 @@
-package JavaBrain.concurrency;public class MyRunnable {
+package JavaBrain.concurrency;
+
+public class MyRunnable implements Runnable{
+    public void run() {
+        System.out.println("I am running");
+    }
+
+    public static void main(String[] args) {
+        Runnable r = new MyRunnable();
+        Thread t = new Thread(r);
+        t.start();
+
+        // alternatives.
+        Runnable r2 = () -> System.out.println("Running");
+
+        // or
+        new Thread(() -> System.out.println()).start();
+    }
+
 }
